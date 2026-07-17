8bd24e35d708578050c9199ee5afa5485e90a69a
coodie1 <umairarif946@gmail.com>
2026-07-17 11:05:02 +0500

    Problem no.141_Detect_Cycle_in_Linked_List

---
+++ LinkedList/141_Detect_Cycle_Linked_List.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast){
+                return true;
+            }
+        }
+
+        return false;   //false loop ka bad return krna loop ka andr return kiya tu shoro ma ee return false ho jay ga
+
+    }
+};
